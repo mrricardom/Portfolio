6f9ef83d377ff5ed9b413cb4660638d47751e250
Ricardo Martini <Partyandhot@Ricardos-MBP.fios-router.home>
2021-03-25 21:53:07 -0400

    changed mediaqueries for screens under 900px

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function toggleClass() {
-  let menu = document.querySelector('.right-nav')
-  menu.classList.toggle('toggleHam')
-}
+// function toggleClass() {
+//   let menu = document.querySelector('.right-nav')
+//   menu.classList.toggle('collapse')
+// }
 
-let hamburger = document.querySelector('.hamIcon')
+// let hamburger = document.querySelector('.hamIcon')
 
-hamburger.addEventListener('click', toggleClass)
+// hamburger.addEventListener('click', toggleClass)
